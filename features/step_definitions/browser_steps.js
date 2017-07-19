7a1f78bd5d6aab84601c2fc5eb01f2a3d410cdf9
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-07-19 16:55:38 +0300

    add change in steps

--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -8,46 +8,44 @@ var EC = new protractor.ProtractorExpectedConditions();
 defineSupportCode(function({Given, When, Then}) {
 
 	Given('I am going to the Onliner.by', function() {
-		// return this.browser.driver.get('https://www.onliner.by/');
 		return this.browser.get('https://www.onliner.by/');
 	});
 
 	When('I click on mobile phones', function() {
 		let locator = '.project-navigation__sign';
-        let text = 'Мобильные телефоны'
+		let text = 'Мобильные телефоны'
 		return this.browser.element(by.cssContainingText(locator, text)).click();
 	});
 
-    When('I set filter Apple', function() {
-        let locator = '.schema-filter__checkbox-text';
-        let text = 'Apple';
-        return this.browser.element.all(by.cssContainingText('span', text)).first().click();
-    });
+	When('I set filter Apple', function() {
+		let locator = '.schema-filter__checkbox-text';
+		let text = 'Apple';
+		return this.browser.element.all(by.cssContainingText('span', text)).first().click();
+	});
 
-    When('Valid set filter {stringInDoubleQuotes}', function(text) {
-        var condition = this.browser.element(by.cssContainingText('div.schema-tags__item > span', text));
-        return this.browser.wait(EC.presenceOf(condition), 5000);
-    });
+	When('Valid set filter {stringInDoubleQuotes}', function(text) {
+		var condition = this.browser.element(by.cssContainingText('div.schema-tags__item > span', text));
+		return this.browser.wait(EC.presenceOf(condition), 5000);
+	});
 
-    When('I find {stringInDoubleQuotes}', function(text) {
-        // this.browser.sleep(5000);
-        var wantedElement = this.browser.element(by.cssContainingText('span', text));
-        this.browser.wait(EC.elementToBeClickable(wantedElement), 5000);
-        this.browser.executeScript("arguments[0].scrollIntoView();", wantedElement);
-        return wantedElement.click();
-    });
+	When('I find {stringInDoubleQuotes}', function(text) {
+		var wantedElement = this.browser.element(by.cssContainingText('span', text));
+		this.browser.wait(EC.elementToBeClickable(wantedElement), 5000);
+		this.browser.executeScript("arguments[0].scrollIntoView();", wantedElement);
+		return wantedElement.click();
+	});
 
-    When('Come back to mobile page', function () {
-        return this.browser.navigate().back();
-    })
+	When('Come back to mobile page', function() {
+		return this.browser.navigate().back();
+	})
 
-    When('Click add to compare phone', function () {
-        return this.browser.element(by.cssContainingText('span', 'Добавить к сравнению')).click();
-    });
+	When('Click add to compare phone', function() {
+		return this.browser.element(by.cssContainingText('span', 'Добавить к сравнению')).click();
+	});
 
-    When('Going to compare page', function () {
-        this.browser.element.all(by.css('a.compare-button__sub.compare-button__sub_main')).first().click();
-        return this.browser.sleep(5000);
-    })
+	When('Going to compare page', function() {
+		this.browser.element.all(by.css('a.compare-button__sub.compare-button__sub_main')).first().click();
+		return this.browser.sleep(5000);
+	})
 
 });
